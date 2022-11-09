563375d9ee844a745b9f185bbd794462059cb23e
Melqui Brito <Merkulino11@gmail.com>
2022-11-09 18:49:33 -0300

    Rendering phrase req02

--- src/Page/Main.jsx
+++ src/Page/Main.jsx
@@ -1,49 +1,51 @@
 import React, { Component } from 'react';
-import { getCategories } from '../services/api';
-import ProductCard from '../Components/ProductCard';
+// import { getCategories } from '../services/api';
+// import ProductCard from '../Components/ProductCard';
 
 class Main extends Component {
-  state = {
-    input: '',
-    products: [],
-  };
+  // state = {
+  //   input: '',
+  //   products: [],
+  // };
 
-  search = async () => {
-    const { input } = this.state;
-    const response = await getCategories(input);
-    this.setState({
-      products: response,
-    });
-  };
+  // search = async () => {
+  //   const { input } = this.state;
+  //   const response = await getCategories(input);
+  //   this.setState({
+  //     products: response,
+  //   });
+  // };
 
-  handleChange = ({ target }) => {
-    const { value } = target;
-    this.setState({
-      input: value,
-    });
-  };
+  // handleChange = ({ target }) => {
+  //   const { value } = target;
+  //   this.setState({
+  //     input: value,
+  //   });
+  // };
 
   render() {
-    const { input, products } = this.state;
+    // const { input, products } = this.state;
     return (
       <form>
         <label htmlFor="home-initial-message">
           <input
-            data-testid="home-initial-message"
-            onChange={ this.handleChange }
+            // onChange={ this.handleChange }
             placeholder="Search"
-            value={ input }
+            // value={ input }
             type="text"
             id="home-initial-message"
           />
           <button
             type="submit"
-            onClick={ this.search }
+            // onClick={ this.search }
           >
             Search
           </button>
         </label>
-        <ul>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        {/* <ul>
           {products.length > 0
             ? products.map((productCard) => (
               <ProductCard
@@ -51,7 +53,7 @@ class Main extends Component {
                 product={ productCard }
               />))
             : 'Digite algum termo de pesquisa ou escolha uma categoria.'}
-        </ul>
+        </ul> */}
       </form>
     );
   }
